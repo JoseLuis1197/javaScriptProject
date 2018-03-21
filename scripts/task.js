fa9a4a37f1bb25aa9678951cc2e1bd43270d4790
Jose Proaño <jlps_94_11@hotmail.com>
2018-03-21 16:11:48 -0500

    fix(script): updating

--- scripts/task.js
+++ scripts/task.js
@@ -1,10 +1,25 @@
 //create a task object
-
+/*
 var task = {};
 
 task.title = "My task";
 task.description = "My description";
+task.toString = function(){
+    return this.title + " " + this.description;        
+}
 
-console.log(task);
+console.log(task.toString());
 
+*/
+
+//create a task object
+
+var task = {
+    title : "My task",
+    description : "My description",
+    toString : function(){
+    return this.title + " " + this.description 
+    }    
+};
     
+console.log(task.toString());
